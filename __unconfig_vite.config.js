c4c7bf51e221a55fbeeaf88301b9b053fd1d7256
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-02-22 23:23:48 +0530

    Updated version

---
+++ __unconfig_vite.config.js
@@ -0,0 +1,13 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+const __unconfig_default =  defineConfig({
+  plugins: [svelte()]
+})
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
